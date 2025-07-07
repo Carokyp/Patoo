2948a222a5664383b116fe404937226924d0499c
Carokyp <carolinekypreos@outlook.com>
2025-07-07 13:49:17 +0100

    push

--- Assets/Institatial.cs
+++ Assets/Institatial.cs
@@ -9,7 +9,7 @@ public class Institatial : MonoBehaviour
 #if UNITY_ANDROID
     private string _adUnitId = "ca-app-pub-1377152838708261/2075221693";
 #elif UNITY_IPHONE
-    private string _adUnitId = "ca-app-pub-1377152838708261/9263446572";
+    private string _adUnitId = "ca-app-pub-3940256099942544/4411468910";
 #else
   private string _adUnitId = "unused";
 #endif
